54feff3fbe2c8a00bad49202415b7b264b300b9b
Ryan <rykelly@cs.washington.edu>
2024-05-24 16:37:21 -0700

    Basic lattice type hierarchy

---
+++ src/main/java/org/checkerframework/checker/dividebyzero/qual/Bottom.java
@@ -0,0 +1,15 @@
+package org.checkerframework.checker.dividebyzero.qual;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+import org.checkerframework.framework.qual.SubtypeOf;
+
+
+/**
+ * You do not want a qualifier `@Top`. It appears here as a placeholder, so that the project
+ * compiles.
+ */
+@SubtypeOf({Negative.class, Zero.class, Positive.class})
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+public @interface Bottom {}
