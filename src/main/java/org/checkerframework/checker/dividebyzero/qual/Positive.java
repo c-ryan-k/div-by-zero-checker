eebaf0ada28a7f44ce809baebc2f987ae32fff4e
Ryan <rykelly@cs.washington.edu>
2024-05-24 16:37:21 -0700

    Added Nonzero lattice point

--- src/main/java/org/checkerframework/checker/dividebyzero/qual/Positive.java
+++ src/main/java/org/checkerframework/checker/dividebyzero/qual/Positive.java
@@ -8,6 +8,6 @@ import org.checkerframework.framework.qual.SubtypeOf;
  * You do not want a qualifier `@Top`. It appears here as a placeholder, so that the project
  * compiles.
  */
-@SubtypeOf({Top.class})
+@SubtypeOf({NonZero.class})
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface Positive {}
